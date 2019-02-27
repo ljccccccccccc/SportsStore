8126a0a7a7fafa8be595fba93174f18dd993faaa
ljccccccccccc <ljccccccccccc@163.com>
2019-02-27 19:53:53 +0800

    nova 2019-02-27

---
+++ data.js
@@ -0,0 +1,16 @@
+module.exports = function (){
+  return {
+    products : [
+      {id:1,name:"Kayak",category:"WaterSports",description:"A boat for one person",price:275},
+      {id:2,name:"Joe",category:"WaterSports",description:"be removed soon",price:286},
+      {id:3,name:"Mike",category:"WaterSports",description:"hello my darling,dear",price:234},
+      {id:4,name:"Smith Joke",category:"WaterSports",description:"Beef vegetables",price:4355},
+      {id:5,name:"Hash Map",category:"WaterSports",description:"You can you up ",price:2456},
+      {id:6,name:"Java Scripts",category:"WaterSports",description:"No can no BB",price:256},
+      {id:7,name:"C++ Enter Press",category:"WaterSports",description:"It's nice to meet you ",price:275},
+      {id:8,name:"King of the world",category:"WaterSports",description:"Hello MR.DJ would you like some juice",price:235},
+      {id:9,name:"Super me",category:"WaterSports",description:"A Dog",price:275}
+    ],
+    order : []
+  }
+};
